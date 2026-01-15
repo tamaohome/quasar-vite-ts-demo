94d49497048f89d4cb4aaa39c9ba723ef03cc022
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-01-15 15:28:49 +0900

    💎 style: vueのスタイル修正

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,10 @@
+<script setup lang="ts"></script>
+
 <template>
   <q-card v-bind="$attrs" class="custom-rounded-card q-pa-lg">
     <slot />
   </q-card>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped lang="scss">
 .custom-rounded-card {
