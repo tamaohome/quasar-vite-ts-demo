1d405b517b48d4659b5db47e76739cefa7dd9a60
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-01-15 10:47:41 +0900

    🏗️ build: quasarの設定ファイルを適用

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { Quasar } from "quasar";
+import quasarLang from "quasar/lang/ja";
 
-createApp(App).mount("#app");
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
+import "@/style.scss";
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from "@/App.vue";
+
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+});
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  plugins: [vue()],
+
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: fileURLToPath(new URL("./src/quasar-variables.sass", import.meta.url)),
+    }),
+  ],
 });
